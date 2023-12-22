7795a8313ba75e95278a894ffce4160f4020aee8
Geovanny Mendoza <geovanny0401@gmail.com>
2023-12-21 22:07:41 -0500

    [V-1]Integration bootstrap with app

---
+++ src/main/java/com/geovannycode/cakefactory/CakefactoryApplication.java
@@ -0,0 +1,13 @@
+package com.geovannycode.cakefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakefactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakefactoryApplication.class, args);
+	}
+
+}
